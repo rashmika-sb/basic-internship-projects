415212b3fdfe4faf651ec26cd0562183bfec54ee
Rashmika SB <rashmikasb22@gmail.com>
2023-04-02 22:03:36 +0530

    intial commit

--- README.md
+++
@@ -1,6 +0,0 @@
-hello,
-basic-internship-projects done using html, css, scss and javascript.
-
-all the projects lies in their own branches.
-
-branches:
---
+++ script.js
@@ -0,0 +1,16 @@
+const insert = document.getElementById("insert");
+
+window.addEventListener("keydown", (event) => {
+  insert.innerHTML = `<div class="key">
+    ${event.key === " " ? "space" : event.key}
+    <small>Event.key</small>
+  </div>
+  <div class="key">
+    ${event.keyCode}
+    <small>Event.keyCode</small>
+  </div>
+  <div class="key">
+    ${event.code}
+    <small>Event.code</small>
+  </div>`;
+});
